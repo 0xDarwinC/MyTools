1bb036f14efaaa1509901ae11a24d747837b3aec
0xDarwinC <darwinchris.edu0525@gmail.com>
2026-06-03 01:17:00 -0400

    removed unecessary templates from smtsolver

--- SMTSolver.cpp
+++ SMTSolver.cpp
@@ -1,67 +1,7 @@
 #include "bits/stdc++.h"
 #define int long long
-#define uint unsigned long long
-#define vi vector<int>
-#define vvi vector<vi>
-#define vb vector<bool>
-#define vvb vector<vb>
-#define pi pair<int, int>
-#define F first
-#define S second
 #define pb push_back
-#define mp make_pair
-#define fr(i, n) for (int i = 0; i < (n); i++)
-#define rep(i, a, n) for (int i = (a); i <= (n); i++)
-#define nl "\n"
-#define dbg(var) cerr << #var << "=" << var << " "
-#define all(v) v.begin(), v.end()
-#define srt(v) sort(v.begin(), v.end())         // sort
-#define mxe(v) *max_element(v.begin(), v.end()) // find max element in vector
-#define mne(v) *min_element(v.begin(), v.end()) // find min element in vector
-#define unq(v) v.resize(distance(v.begin(), unique(v.begin(), v.end())));
-// make sure to sort before applying unique // else only consecutive duplicates
-// would be removed
-#define bin(x, y) bitset<y>(x)
 using namespace std;
-int MOD = 1e9 + 7; // Hardcoded, directly change from here for functions!
-
-const int MX_SZ = 1e5 + 5;
-int par[MX_SZ];
-
-void modadd(int &a, int b) { a = ((a % MOD) + (b % MOD)) % MOD; }
-void modsub(int &a, int b) { a = ((a % MOD) - (b % MOD) + MOD) % MOD; }
-void modmul(int &a, int b) { a = ((a % MOD) * (b % MOD)) % MOD; }
-// ================================== take ip/op like vector,pairs
-// directly!==================================
-template <typename typC, typename typD>
-istream &operator>>(istream &cin, pair<typC, typD> &a) {
-  return cin >> a.first >> a.second;
-}
-template <typename typC> istream &operator>>(istream &cin, vector<typC> &a) {
-  for (auto &x : a)
-    cin >> x;
-  return cin;
-}
-template <typename typC, typename typD>
-ostream &operator<<(ostream &cout, const pair<typC, typD> &a) {
-  return cout << a.first << ' ' << a.second;
-}
-template <typename typC, typename typD>
-ostream &operator<<(ostream &cout, const vector<pair<typC, typD>> &a) {
-  for (auto &x : a)
-    cout << x << '\n';
-  return cout;
-}
-template <typename typC>
-ostream &operator<<(ostream &cout, const vector<typC> &a) {
-  int n = a.size();
-  if (!n)
-    return cout;
-  cout << a[0];
-  for (int i = 1; i < n; i++)
-    cout << ' ' << a[i];
-  return cout;
-}
 // ===================================END Of the input module
 // ==========================================
 
@@ -93,14 +33,14 @@ signed main() {
 
     char color;
     while (ss >> color) {
-      c.colors.push_back(color);
+      c.colors.pb(color);
 
       // Track the unique colors used in this puzzle
       if (unique_colors.find(color) == string::npos) {
         unique_colors += color;
       }
     }
-    constraints.push_back(c);
+    constraints.pb(c);
   }
 
   if (unique_colors.length() != 5) {
@@ -116,7 +56,6 @@ signed main() {
   // iterate through all 120 possible permutations
   do {
     bool valid = true;
-
     for (const auto &c : constraints) {
       int matches = 0;
       for (int i = 0; i < 5; ++i) {
@@ -136,18 +75,14 @@ signed main() {
       for (char color : unique_colors) {
         cout << color << " ";
       }
-      cout << "\n";
       found = true;
       break;
     }
-
   } while (next_permutation(unique_colors.begin(), unique_colors.end()));
 
   if (!found) {
     cout << "No valid permutation found. Check constraints.\n";
   }
-
-  return 0;
 
   return 0;
 }
